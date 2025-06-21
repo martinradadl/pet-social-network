96457ab9cff6a90a4c2aaa6e8bac4a64a3d21a98
Martin Rada <martinradadl@gmail.com>
2025-06-20 19:29:22 -0500

    New screens added to navigator

--- src/main-router.tsx
+++ src/main-router.tsx
@@ -1,17 +1,28 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { Home } from './pages/home';
+import {Home} from './pages/home';
+import {Profile} from './pages/profile';
+import {Footer} from './components/footer';
 
-const Stack = createNativeStackNavigator();
+export type RootStackScreensList = {
+  Home: undefined;
+  Profile: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackScreensList>();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <React.Fragment>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Profile" component={Profile} />
+        </Stack.Navigator>
+        <Footer />
+      </NavigationContainer>
+    </React.Fragment>
   );
 };
 
