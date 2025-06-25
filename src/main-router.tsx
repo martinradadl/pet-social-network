cf4117914149ddfc45d83f823667709174dc6692
Martin Rada <martinradadl@gmail.com>
2025-06-25 18:39:34 -0500

    Main Router updated

--- src/main-router.tsx
+++ src/main-router.tsx
@@ -1,26 +1,38 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Home} from './pages/home';
-import {Profile} from './pages/profile';
 import {Footer} from './components/footer';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Login} from './pages/auth/login';
+import {SignUp} from './pages/auth/sign-up';
 
 export type RootStackScreensList = {
-  Home: undefined;
-  Profile: undefined;
+  Login: undefined;
+  SignUp: undefined;
+  Footer: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackScreensList>();
 
 const App = () => {
+  const isAuthenticated = true;
+
   return (
     <React.Fragment>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Profile" component={Profile} />
-        </Stack.Navigator>
-        <Footer />
+        {isAuthenticated ? (
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Footer"
+              component={Footer}
+              options={{headerShown: false}}
+            />
+          </Stack.Navigator>
+        ) : (
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+          </Stack.Navigator>
+        )}
       </NavigationContainer>
     </React.Fragment>
   );
