316beee1030dfe1c29452115cb991e75a3ea004d
Martin Rada <martinradadl@gmail.com>
2025-07-30 12:58:11 -0500

    new screens and user authentication logic added to main router

--- src/main-router.tsx
+++ src/main-router.tsx
@@ -4,17 +4,35 @@ import {Footer} from './components/footer';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Login} from './pages/auth/login';
 import {SignUp} from './pages/auth/sign-up';
+import {SignUpCompleted} from './pages/auth/sign-up-completed';
+import {ForgotPassword} from './pages/auth/forgot-password';
+import {useAuth} from './data/auth';
+import {useShallow} from 'zustand/react/shallow';
 
 export type RootStackScreensList = {
   Login: undefined;
   SignUp: undefined;
+  SignUpCompleted: undefined;
   Footer: undefined;
+  ForgotPassword: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackScreensList>();
 
 const App = () => {
-  const isAuthenticated = true;
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+  const {user} = useAuth(
+    useShallow(state => ({
+      user: state.user,
+    })),
+  );
+
+  React.useEffect(() => {
+    if (user?._id) {
+      setIsAuthenticated(true);
+    }
+  }, [user?._id]);
 
   return (
     <React.Fragment>
@@ -28,9 +46,27 @@ const App = () => {
             />
           </Stack.Navigator>
         ) : (
-          <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Navigator initialRouteName="SignUpCompleted">
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="SignUp"
+              component={SignUp}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="SignUpCompleted"
+              component={SignUpCompleted}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="ForgotPassword"
+              component={ForgotPassword}
+              options={{headerShown: false}}
+            />
           </Stack.Navigator>
         )}
       </NavigationContainer>
