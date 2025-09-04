2fef56c62782dc30d792086673d761cbb67db87d
Martin Rada <martinradadl@gmail.com>
2025-09-04 13:26:50 -0500

    New screens for each profile option created

---
+++ src/pages/profile/content-preferences/archive.tsx
@@ -0,0 +1,21 @@
+import {ScrollView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {ScreenHeader} from '../../../components/header';
+
+export const Archive = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScreenHeader title="Stories archive" />
+      <ScrollView>
+        <Text>Content</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
